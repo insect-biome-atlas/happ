2518a4a80686cc086a77c091e1a6abb788c3d5ad
johnne <john.sundh@scilifelab.se>
2022-12-06 10:25:36 +0100

    Add functionality for calculating order-level stats

--- workflow/scripts/evaluate_clusters.py
+++ workflow/scripts/evaluate_clusters.py
@@ -26,7 +26,8 @@ def merge_cluster_df(clustdf, taxdf):
     """
     Merge cluster dataframe with taxonomy
     """
-    dataf = pd.merge(clustdf, taxdf, left_index=True, right_index=True, how="inner")
+    dataf = pd.merge(clustdf, taxdf, left_index=True, right_index=True,
+                     how="inner")
     dataf["cluster_Family"] = dataf["cluster"] + dataf["Family"]
     return dataf
 
@@ -46,51 +47,51 @@ def truepos(res, rank):
     return res.groupby(rank).apply(pairs)
 
 
-def falseNegatives(df, cluster_col, rank):
+def falseNegatives(df, cluster_col, rank, silent=False):
     """
     Iterates each unique label (e.g. species) and calculates how many that should
     be clustered but are not
     """
-    cl_rank_size = pd.DataFrame(df.groupby([cluster_col, rank]).size()).reset_index()
+    cl_rank_size = pd.DataFrame(
+        df.groupby([cluster_col, rank]).size()).reset_index()
     FN = 0
-    for doc in tqdm(
-        cl_rank_size[rank].unique(),
-        desc="finding false negatives",
-        ncols=10,
-        dynamic_ncols=True,
-        unit=f" {rank}",
-    ):
+    if not silent:
+        iter = tqdm(cl_rank_size[rank].unique(), desc="finding false negatives",
+                ncols=10, dynamic_ncols=True, unit=f" {rank}")
+    else:
+        iter = cl_rank_size[rank].unique()
+    for doc in iter:
         items = list(cl_rank_size.loc[cl_rank_size[rank] == doc, 0].items())
         for i, item in enumerate(items):
-            FN += item[1] * sum([x[1] for x in items[i + 1 :]])
+            FN += item[1] * sum([x[1] for x in items[i + 1:]])
     return FN
 
 
-def precision_recall(df, cluster_col, rank):
+def precision_recall(df, cluster_col, rank, silent=False):
     """
     Calculates true positives, false positives, false negatives and returns
     precision and recall
     """
     totalClusters = len(df[cluster_col].unique())
-    sys.stdout.write(f"Total clusters {totalClusters}\n")
     totalTaxa = len(df[rank].unique())
-    sys.stdout.write(f"Total number of {rank} {totalTaxa}\n")
     totalPositives = sum(df.groupby(cluster_col).apply(pairs))
-    sys.stdout.write(f"Total positives: {totalPositives}\n")
-    # results/opticlust/MBs_merged/Polyxenidae/params2/asv_clusters.tsv 1 Species
     if totalTaxa == 1:
         TP = df.groupby(cluster_col).apply(truepos, rank=rank).sum().values[0]
     else:
         TP = df.groupby(cluster_col).apply(truepos, rank=rank).sum()
-    sys.stdout.write(f"True positives: {TP}\n")
     FP = totalPositives - TP
-    sys.stdout.write(f"False positives: {FP}\n")
     # totalNegatives = pairs(df) - totalPositives
-    FN = falseNegatives(df, cluster_col, rank)
-    sys.stdout.write(f"False negatives: {FN}\n")
+    FN = falseNegatives(df, cluster_col, rank, silent)
     # TN = totalNegatives - FN
     precision = float(TP) / (TP + FP)
     recall = float(TP) / (TP + FN)
+    if not silent:
+        sys.stdout.write(f"Total clusters {totalClusters}\n")
+        sys.stdout.write(f"Total number of {rank} {totalTaxa}\n")
+        sys.stdout.write(f"Total positives: {totalPositives}\n")
+        sys.stdout.write(f"True positives: {TP}\n")
+        sys.stdout.write(f"False positives: {FP}\n")
+        sys.stdout.write(f"False negatives: {FN}\n")
     return precision, recall
 
 
@@ -102,19 +103,35 @@ def homcom(clustdf, clust_col, rank):
     return homogeneity, completeness
 
 
+def calc_order_level(clustdf, rank):
+    order_level = {}
+    orders = clustdf.Order.unique()
+    for o in tqdm(orders, desc="calculating stats", ncols=10,
+                  dynamic_ncols=True, unit=f" Order", ):
+        _dataf = clustdf.loc[clustdf.Order == o]
+        n_rank = len(_dataf[rank].unique())
+        n_asv = _dataf.shape[0]
+        p, r = precision_recall(_dataf, "cluster_Family", rank, silent=True)
+        h, c = homcom(_dataf, "cluster_Family", rank)
+        order_level[o] = {'precision': p, 'recall': r, 'homogeneity': h,
+                          'completeness': c, rank: n_rank, 'ASVs': n_asv}
+    order_leveldf = pd.DataFrame(order_level).T
+    order_leveldf.index.name = "Order"
+    return order_leveldf
+
+
 def main(args):
     # Read taxonomies
     asv_taxa = pd.read_csv(args.taxfile, sep="\t", index_col=0)
     sys.stderr.write(f"#{asv_taxa.shape[0]} ASV taxonomies loaded\n")
     # Remove ASVs without assignments for args.rank
     sys.stderr.write(f"#Removing ASVs without assignments for {args.rank}\n")
-    asv_taxa = asv_taxa.loc[
-        (~asv_taxa[args.rank].str.contains("_X+$"))
-        & (~asv_taxa[args.rank].str.startswith("unclassified."))
-    ]
+    asv_taxa = asv_taxa.loc[(~asv_taxa[args.rank].str.contains("_X+$")) & (
+        ~asv_taxa[args.rank].str.startswith("unclassified."))]
     sys.stderr.write(f"#{asv_taxa.shape[0]} ASVs remaining\n")
     # Read cluster files
-    sys.stderr.write(f"#Loading cluster results from {len(args.clustfiles)} files\n")
+    sys.stderr.write(
+        f"#Loading cluster results from {len(args.clustfiles)} files\n")
     clustdf = read_asv_clusters(args.clustfiles)
     # Merge with taxonomies
     sys.stderr.write("#Merging with taxonomic assignments\n")
@@ -122,30 +139,30 @@ def main(args):
     sys.stderr.write(f"#{clustdf.shape[0]} ASVs remaining after merging\n")
     if clustdf.shape[0] == 0:
         sys.exit("Not enough data to evaluate\n")
+    if args.order_level:
+        sys.stderr.write(
+            f"Calculating statistics for {len(clustdf.Order.unique())} orders\n")
+        order_leveldf = calc_order_level(clustdf, args.rank)
+        sys.stderr.write(f"Writing order-level results to {args.order_level}\n")
+        order_leveldf.to_csv(args.order_level, sep="\t")
     precision, recall = precision_recall(clustdf, "cluster_Family", args.rank)
     homogeneity, completeness = homcom(clustdf, "cluster_Family", args.rank)
     sys.stdout.write(f"precision\t{precision}\nrecall\t{recall}\n")
-    sys.stdout.write(f"homogeneity\t{homogeneity}\ncompleteness\t{completeness}\n")
+    sys.stdout.write(
+        f"homogeneity\t{homogeneity}\ncompleteness\t{completeness}\n")
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument(
-        "taxfile", type=str, help="File with taxonomic assignments for ASVs"
-    )
-    parser.add_argument(
-        "clustfiles",
-        type=str,
-        nargs="+",
+    parser.add_argument("taxfile", type=str,
+        help="File with taxonomic assignments for ASVs")
+    parser.add_argument("clustfiles", type=str, nargs="+",
         help="Cluster membership file. Should be tab-separated,"
-        " have a header, and contain ASV ids in the first column and "
-        "cluster name in second column",
-    )
-    parser.add_argument(
-        "--rank",
-        type=str,
-        default="Species",
-        help="Evaluate clusters against a 'true' cluster rank",
-    )
+             " have a header, and contain ASV ids in the first column and "
+             "cluster name in second column", )
+    parser.add_argument("--rank", type=str, default="Species",
+        help="Evaluate clusters against a 'true' cluster rank", )
+    parser.add_argument("--order_level", type=str,
+                        help="Calculate order-level stats and write to this file")
     args = parser.parse_args()
     main(args)
