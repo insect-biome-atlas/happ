2fa1688b5202612d347e14c5ad89edcf34fa16fb
johnne <john.sundh@scilifelab.se>
2024-05-06 21:35:51 +0200

    Handle orders with only one cluster

--- workflow/scripts/numt_filtering/combined_filter.R
+++ workflow/scripts/numt_filtering/combined_filter.R
@@ -22,6 +22,9 @@ order_name <- snakemake@wildcards$order
 spikeins <- readLines(snakemake@input$spikeins)
 n_closest <- snakemake@params$n_closest
 
+## OUTPUT
+output <- snakemake@output$tsv
+
 ## INPUT
 fasta <- snakemake@input$fasta
 
@@ -30,11 +33,6 @@ taxdf <- read.table(snakemake@input$taxonomy, sep="\t", header=TRUE, check.names
 # Subset taxonomy to order name
 taxonomy <- taxdf[taxdf$Order==order_name,]
 
-# Add spikeins if not null
-if (length(spikeins) > 0) {
-     spikes <- taxdf[taxdf$cluster %in% spikeins,]
-     taxonomy <- rbind(taxonomy, spikes)
-}
 countsfile <- snakemake@input$counts
 cat("Reading cluster counts\n")
 counts <- fread(countsfile, check.names=FALSE, nThread=threads, 
@@ -42,8 +40,29 @@ counts <- fread(countsfile, check.names=FALSE, nThread=threads,
 # Subset to clusters defined for order
 counts <- counts[counts$cluster %in% taxonomy$cluster,]
 
-## OUTPUT
-output <- snakemake@output$tsv
+# Handle cases where there is only one cluster in the order
+if (nrow(taxonomy) == 1) {
+     cat("Only one cluster in order\n")
+     res <- data.table(matrix(NA, nrow = 1, ncol = 5))
+     colnames(res) <- c("cluster", "n_samples", "n_reads", "numt", "reason")
+     res$cluster <- taxonomy$cluster
+     res$n_samples <- apply(counts[,2:ncol(counts)] > 0, 1, sum)
+     res$n_reads <- sum(counts[2:ncol(counts)])
+     res$numt <- "FALSE"
+     res$reason <- ""
+     write.table(res, snakemake@output$tsv, sep="\t", quote=FALSE, row.names=FALSE)
+     sink()
+     quit(save="no", status=0, runLast=FALSE)
+}
+res <- data.table(matrix(NA, nrow = 1, ncol = 5))
+colnames(res) <- c("cluster", "n_samples", "n_reads", "numt", "reason")
+
+# Add spikeins if not null
+if (length(spikeins) > 0) {
+     spikes <- taxdf[taxdf$cluster %in% spikeins,]
+     taxonomy <- rbind(taxonomy, spikes)
+}
+
 res <- combined_filter_neighbors(fasta=fasta, taxonomy=taxonomy, counts=counts, output=output, 
                         spikeins=spikeins, n_closest=n_closest)
 sink()
