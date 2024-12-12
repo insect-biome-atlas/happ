5c34cab7c416b93f0c85f53eb2b9e7e516807a1e
John Sundh <john.sundh@scilifelab.se>
2024-12-12 01:03:20 +0100

    Output likelihood weight values for gappa

--- workflow/scripts/gappa2taxdf.py
+++ workflow/scripts/gappa2taxdf.py
@@ -4,22 +4,18 @@ from argparse import ArgumentParser
 
 
 def main(args):
-    header = "\t".join(["ASV"] + args.ranks)
+    header = "\t".join(["ASV"] + args.ranks + ["LWR", "fract", "aLWR", "afract"])
     with open(args.infile, "r") as fhin, open(args.outfile, "w") as fhout:
         for i, line in enumerate(fhin):
             if i == 0:
                 fhout.write(f"{header}\n")
                 continue
             name, lwr, fract, alwr, afract, taxopath = line.rstrip().split("\t")
-            lwr = float(lwr)
-            fract = float(fract)
-            alwr = float(alwr)
-            afract = float(afract)
             taxnames = taxopath.split(";")
             taxnames += [f"unclassified.{taxnames[-1]}"] * (
                     len(args.ranks) - len(taxnames)
             )
-            line_out = "\t".join([name] + taxnames)
+            line_out = "\t".join([name] + taxnames + [lwr, fract, alwr, afract])
             fhout.write(f"{line_out}\n")
 
 
