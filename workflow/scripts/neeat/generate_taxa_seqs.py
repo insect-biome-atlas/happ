6eafb422a3756eba0a88b562d3748ddf810a76ae
John Sundh <john.sundh@scilifelab.se>
2025-07-06 05:35:48 +0200

    Update handling of fasta headers
    Handle cases where the fasta header contains an ASV identifier as the id and cluster name in description, as well as cases where the sequence id is the fasta id

--- workflow/scripts/neeat/generate_taxa_seqs.py
+++ workflow/scripts/neeat/generate_taxa_seqs.py
@@ -2,50 +2,68 @@ import subprocess
 import pandas as pd
 from argparse import ArgumentParser
 import os
+import sys
 
+def check_fasta_header(f):
+    with open(f, 'r') as fhin:
+        line = fhin.readline()
+    return line.rstrip().lstrip(">").split(" ")
 
 def main(args):
     os.makedirs(args.outdir, exist_ok=True)
     # read in the taxonomy file
     taxdf = pd.read_csv(args.taxfile, sep="\t", index_col=0)
-    # extract representative ASVs
-    taxdf = taxdf.loc[taxdf["representative"] == 1]
+    # extract representative sequences
+    if "representative" in taxdf.columns:
+        taxdf = taxdf.loc[taxdf.representative==1]
+    taxdf.index.name="seqid"
+    if "cluster" in taxdf.columns:
+        taxdf = taxdf.reset_index().set_index("cluster")
     # also make sure that only clusters that remain in the counts file are used
     clusters_w_counts = []
     with open(args.countsfile, "r") as fhin:
-        for line in fhin:
-            clusters_w_counts.append(line.split("\t")[0])
-    taxdf = taxdf.loc[taxdf["cluster"].isin(clusters_w_counts)]
+        clusters_w_counts = [line.split("\t")[0] for line in fhin]
+    taxdf = taxdf.loc[taxdf.index.isin(clusters_w_counts)]
     # taxa is the unique set of values for split_rank
     taxa = taxdf[args.rank].unique()
     singles = pd.DataFrame()
+    header = check_fasta_header(args.fastafile)
+    # if fasta header only contains one identifier
+    # we assume this identifier is also in the taxdf index
+    if len(header) == 1 and header[0] in taxdf.index:
+        cluster_desc = False
+    # otherwise we assume the second identifier in the header matches
+    # the taxdf index
+    elif len(header) > 1 and header[1] in taxdf.index:
+        cluster_desc = True
+    else:
+        sys.exit("Cannot match fasta headers to taxfile")
     # iterate over taxa
     for tax in taxa:
         # get asvs and cluster designation
-        asvs = taxdf.loc[taxdf[args.rank] == tax, "cluster"]
+        seqs = taxdf.loc[taxdf[args.rank] == tax]
         # skip taxa with fewer than 2 ASVs
-        if len(asvs) < 2:
-            singles = pd.concat([singles, asvs])
+        if len(seqs) < 2:
+            singles = pd.concat([singles, seqs])
             continue
-        # generate strings with '<asv> <cluster>' format to match fasta headers
-        asvs = [f"{x[0]} {x[1]}" for x in list(zip(asvs.index, asvs))]
+        cmd = f"seqkit grep -f {args.outdir}/{tax}.ids -n --quiet {args.fastafile}"
+        seqs = seqs.index.tolist()
+        if cluster_desc:
+            # generate strings with '<asv> <cluster>' format to match fasta headers
+            seqs = [f"{x[0]} {x[1]}" for x in list(zip(taxdf.loc[seqs, "seqid"], seqs))]
+            # add seqkit replace command to strip first id from fasta headers
+            cmd += "| seqkit replace -p '^.+ '"
         # write asvs to file
         with open(f"{args.outdir}/{tax}.ids", "w") as f:
-            _ = f.write("\n".join(asvs))
+            _ = f.write("\n".join(seqs))
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output, _ = process.communicate()
         # use seqkit to extract sequences
         with open(f"{args.outdir}/{tax}.fasta", "w") as fhout:
-            cmd = [
-                "seqkit",
-                "grep",
-                "-f",
-                f"{args.outdir}/{tax}.ids",
-                "-n",
-                "--quiet",
-                args.fastafile,
-            ]
-            _ = subprocess.run(cmd, stdout=fhout)
-            os.remove(f"{args.outdir}/{tax}.ids")
-    singles.to_csv(f"{args.outdir}/singles.tsv", sep="\t")
+            fhout.write(output.decode())
+        os.remove(f"{args.outdir}/{tax}.ids")
+    if len(singles) > 0:
+        singles.to_csv(f"{args.outdir}/singles.tsv", sep="\t")
 
 
 if __name__ == "__main__":
