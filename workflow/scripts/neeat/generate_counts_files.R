bdea61bf483102917c1b78a62e73d9fbb4d6c2dc
John Sundh <john.sundh@scilifelab.se>
2025-07-07 06:38:10 +0200

    Better handling of cluster column

--- workflow/scripts/neeat/generate_counts_files.R
+++ workflow/scripts/neeat/generate_counts_files.R
@@ -32,10 +32,22 @@ if ((meta_file!=FALSE) && file.exists(meta_file)) {
 
 counts <- fread(counts_file, sep="\t")
 counts <- as.data.frame(counts)
+rownames(counts) <- counts[,1]
 taxonomy <- read.delim(taxonomy_file)
 
+# Get representatives
+if ("representative" %in% colnames(taxonomy)) {
+    taxonomy <- taxonomy[taxonomy$representative==1,]
+}
+
 # Extract counts for clusters in taxonomy
-counts <- counts[counts$cluster %in% taxonomy$cluster,]
+if ("cluster" %in% colnames(taxonomy)) {
+    rownames(taxonomy) <- taxonomy$cluster
+} else {
+    rownames(taxonomy) <- taxonomy[,1]
+}
+
+counts <- counts[rownames(taxonomy),]
 
 cat("Generating raw counts table\n")
 # extract counts for samples
